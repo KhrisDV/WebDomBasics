52bd0bdf01834d86eb9fac48aedb33a7c0897a7e
KhrisDV <KhrisQ@protonmail.com>
2022-03-10 09:43:14 +0100

    ex 1 done

--- Case1/js/controllers/main.mjs
+++ Case1/js/controllers/main.mjs
@@ -1,10 +1,8 @@
-export function dataToHTMLList (array) {
-    const HTMLElements = array.map(
-        (item) => {
-            const li = document.createElement("li");
-            li.innerText = `Importe: ${item} puntos.`;
-            return li;
-        }
-    )
-    document.querySelector("#list").append(...HTMLElements);
+export function dataToHTMLList(array) {
+  const HTMLElements = array.map((item) => {
+    const li = document.createElement("li");
+    li.innerText = `Importe: ${item / 2} puntos.`;
+    return li;
+  });
+  document.querySelector("#list").append(...HTMLElements);
 }
