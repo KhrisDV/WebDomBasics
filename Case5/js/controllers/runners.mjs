7ae06f0ffb80102a0fd9c83d9ef81b935118e874
KhrisDV <KhrisQ@protonmail.com>
2022-03-11 09:55:55 +0100

    ex 5 done

--- Case5/js/controllers/runners.mjs
+++ Case5/js/controllers/runners.mjs
@@ -1,12 +1,12 @@
-export function dataToHTMLList (array) {
-    const HTMLElements = array.map(
-        (item) => {
-            const row = document.createElement("tr");
-            const tdName = document.createElement("td");
-            const tdTime = document.createElement("td");
-            row.append(tdName, tdTime);
-            return row;
-        }
-    )
-    document.querySelector("#runnersRows").append(...HTMLElements);
+export function dataToHTMLList(array) {
+  const HTMLElements = array.map((item) => {
+    const row = document.createElement("tr");
+    const tdName = document.createElement("td");
+    const tdTime = document.createElement("td");
+    row.append(tdName, tdTime);
+    tdName.innerText = item.name;
+    tdTime.innerText = item.time;
+    return row;
+  });
+  document.querySelector("#runnersRows").append(...HTMLElements);
 }
