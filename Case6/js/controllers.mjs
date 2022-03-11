2f80aaf6545bea64895024ce185b8c144b13fd1b
KhrisDV <KhrisQ@protonmail.com>
2022-03-11 10:11:27 +0100

    ex 6 done

--- Case6/js/controllers.mjs
+++ Case6/js/controllers.mjs
@@ -1,26 +1,25 @@
 import { getTasks, setTasks } from "./models.mjs";
 
-export function dataToHTMLList (array) {
-    const HTMLElements = array.map(
-        (item) => {
-            const li = document.createElement("li");
-            li.innerText = item.description;
-            if (item.completed) li.classList.add("completed")
-            return li;
-        }
-    )
-    const htmlParent = document.querySelector("#taskList")
-    htmlParent.innerHTML = "";
-    htmlParent.append(...HTMLElements);
+export function dataToHTMLList(array) {
+  const HTMLElements = array.map((item) => {
+    const li = document.createElement("li");
+    li.innerText = item.description;
+    if (item.completed) li.classList.add("completed");
+    return li;
+  });
+  const htmlParent = document.querySelector("#taskList");
+  htmlParent.innerHTML = "";
+  htmlParent.append(...HTMLElements);
+  document
+    .querySelector("button")
+    .addEventListener("click", removeCompletedHandler);
 }
 
-export function removeCompletedHandler (event) {
-    let tasks = getTasks();
-    tasks = tasks.filter(
-        (item) => {
-            return ! item.completed
-        }
-    )
-    setTasks(tasks)
-    dataToHTMLList(tasks)
+export function removeCompletedHandler(event) {
+  let tasks = getTasks();
+  tasks = tasks.filter((item) => {
+    return !item.completed;
+  });
+  setTasks(tasks);
+  dataToHTMLList(tasks);
 }
--- Case6/js/index.mjs
+++ Case6/js/index.mjs
@@ -1,4 +1,4 @@
 import { getTasks } from "./models.mjs";
 import { removeCompletedHandler, dataToHTMLList } from "./controllers.mjs";
 
-dataToHTMLList(getTasks())
+dataToHTMLList(getTasks());
