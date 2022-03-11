9997930bd6575f8531307ec7be926e5db3f84531
KhrisDV <KhrisQ@protonmail.com>
2022-03-11 09:46:41 +0100

    ex 4 done

--- Case4/js/index.mjs
+++ Case4/js/index.mjs
@@ -1,6 +1,9 @@
-function toggleShowCompletedHandler (event) {
-    const completedElements = document.querySelectorAll(".completed");
-    for ( let item of completedElements ) {
-        item.classList.toggle("hidden");
-    }
+function toggleShowCompletedHandler(event) {
+  const completedElements = document.querySelectorAll(".completed");
+  for (let item of completedElements) {
+    item.classList.toggle("hidden");
+  }
 }
+const botonLimpiar = document.querySelector("button");
+
+botonLimpiar.addEventListener("click", toggleShowCompletedHandler);
